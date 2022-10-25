cffdb71d191f3b3ed893525df93f3bbc62815d5d
RayaneKERD <rayane94.kerd@gmail.com>
2022-10-25 11:01:32 +0200

    Caméra

--- app/src/main/java/com/example/foodsafe/MainActivity.java
+++ app/src/main/java/com/example/foodsafe/MainActivity.java
@@ -2,13 +2,46 @@ package com.example.foodsafe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+
+import android.os.Bundle;
+
+import android.provider.MediaStore;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button BtnCam;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BtnCam = (Button) findViewById(R.id.BtnCam);
+        BtnCam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Intent intent = new Intent();
+                    intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivity(intent);
+                }catch(Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
     }
+
+
 }
