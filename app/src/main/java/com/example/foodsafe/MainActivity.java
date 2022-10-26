5fbc128cf338643f001369c9b502fa47e252144c
RayaneKERD <rayane94.kerd@gmail.com>
2022-10-26 21:48:04 +0200

    Menu de navigation fonctionnel

--- app/src/main/java/com/example/foodsafe/MainActivity.java
+++ app/src/main/java/com/example/foodsafe/MainActivity.java
@@ -2,11 +2,11 @@ package com.example.foodsafe;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.app.Activity;
-import android.app.Fragment;
 import android.content.ClipData;
 import android.content.Intent;
 import android.os.Bundle;
@@ -23,15 +23,47 @@ public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
 
-    Button BtnScan;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HistoriqueFragment());
 
+        binding.bottomNavigationView.setOnItemSelectedListener(item ->{
+
+
+            switch (item.getItemId()){
+
+                case R.id.Favoris:
+                    replaceFragment(new FavorisFragment());
+                    break;
+                case R.id.Historique:
+                    replaceFragment(new HistoriqueFragment());
+                    break;
+                case R.id.Scan:
+                    replaceFragment(new ScanFragment());
+                    break;
+            }
+
+
+
+            return true;
+        });
 
     }
+
+    private void replaceFragment(Fragment fragment){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.FrameLayout,fragment);
+        fragmentTransaction.commit();
+    }
+
+
 }
